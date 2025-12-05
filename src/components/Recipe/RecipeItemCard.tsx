d0de6ae7a85172d862850ef91e2ffebf50435d86
shamus osler <shamuso23@gmail.com>
2025-12-04 22:04:29 -0600

    added click functionality for entire RecipeItemCard

--- src/components/Recipe/RecipeItemCard.tsx
+++ src/components/Recipe/RecipeItemCard.tsx
@@ -12,22 +12,19 @@ interface RecipeItemCardProps {
 export function RecipeItemCard({ data, title, ordered = false, defaultExpand = false }: RecipeItemCardProps) {
   const [expanded, setExpanded] = useState(defaultExpand);
   return (
-    <div className="border rounded flex-grow p-4 min-w-60 bg-stone-200 print:bg-stone-100 print:border-gray-300">
+    <div
+      className="border rounded flex-grow p-4 min-w-60 bg-stone-200 print:bg-stone-100 print:border-gray-300 cursor-pointer select-none"
+      onClick={() => setExpanded(!expanded)}>
       <div className="flex justify-between item-center">
         <span className="text-lg">{title}</span>
-        <Button
-          variant="stone"
-          className="print:hidden"
-          onClick={() => setExpanded(!expanded)}>
+        <Button variant="stone" className="print:hidden pointer-events-none">
           {expanded ? <ChevronUp /> : <ChevronDown />}
         </Button>
       </div>
       {expanded ? (
         <ul className="text-sm mt-4 flex flex-col gap-2 print:mt-1 print:gap-1 print:block print:columns-2">
           {data.map((x, i) => (
-            <li
-              key={i}
-              className="mb-1 print:break-inside-avoid">
+            <li key={i} className="mb-1 print:break-inside-avoid">
               {ordered ? `${i + 1}. ${x}` : `- ${x}`}
             </li>
           ))}
@@ -35,9 +32,7 @@ export function RecipeItemCard({ data, title, ordered = false, defaultExpand = f
       ) : (
         <ul className="hidden print:block text-sm mt-4 gap-2 print:mt-1 print:gap-1 print:columns-2">
           {data.map((x, i) => (
-            <li
-              key={i}
-              className="mb-1 print:break-inside-avoid">
+            <li key={i} className="mb-1 print:break-inside-avoid">
               {ordered ? `${i + 1}. ${x}` : `- ${x}`}
             </li>
           ))}
