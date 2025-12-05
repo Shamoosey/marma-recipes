c3f06f72f96d635de249e2fe5babd955c5d41ea7
shamus osler <shamuso23@gmail.com>
2025-12-04 21:12:27 -0600

    Removed github from footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,14 +1,7 @@
 export function Footer() {
   return (
     <footer>
-      <span id="footer-text">
-        &copy; Recipe Tracker {new Date().getFullYear()} - Shamus Osler -{" "}
-        <a
-          className="text-sky-600 hover:underline"
-          href="https://github.com/sosler-rrc/recipe-tracker">
-          Github
-        </a>
-      </span>
+      <span id="footer-text">&copy; Recipe Tracker {new Date().getFullYear()} - Shamus Osler - </span>
     </footer>
   );
 }
